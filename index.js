58086e89a45ae3ec1e839d4a49ab9afedccc31e7
GioCG <informatica@alumno.local>
2024-08-27 07:07:36 -0600

    se agrego proyecto de libreria

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-
-
-const reqCa = fetch(`http://localhost:8088/categoria/`).then((resp)=>{
-  resp.json().then((data)=>{
-    const [{id,nombre}] =data
-    const idDOM = document.getElementById("id")
-    idDOM.innerHTML=id
-    const nombreDom = document.getElementById("nombre")
-    nombreDom.innerHTML = nombre
-    
-    const listaCat =document.getElementById('listaCat')
-    ////////////////////////////////////////////////////////////
-    data.map(({id,nombre}) => {
-      const li = document.createElement('li')
-      li.innerHTML = `ID: ${id} la categoria de ${nombre}`
-      listaCat.appendChild(li)
-    })
-  })  
+const reqCa = fetch('http://localhost:8088/categoria/').then((resp)=>{
+    resp.json().then((data)=>{
+      
+        const listCategoria = document.getElementById('listCategoria')
+ 
+        data.map(({id, nombre})=> {
+            const li = document.createElement('li')
+            li.innerHTML =  `id: ${id} categoria: ${nombre}`
+            listCategoria.appendChild(li)
+        })
+    })  
 }).catch(console.warn)
